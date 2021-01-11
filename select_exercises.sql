1e41ed4106a62ceda858386dd4a035f0d659a374
Correy Everett <correy.everett@gmail.com>
2021-01-11 16:46:39 -0600

    SELECT - Release date of song

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,3 +4,8 @@ SELECT 'Albums by Pink Floyd' AS '';
 SELECT name
 FROM albums
 WHERE artist = 'Pink Floyd';
+
+SELECT 'Release date of Sgt. Pepper''s Lonely Hearts Club Band' AS '';
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
