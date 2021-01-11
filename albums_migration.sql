f8efe5c9b52d9c564eb6bf53071170ecbe62685b
Correy Everett <correy.everett@gmail.com>
2021-01-11 16:45:04 -0600

    create select_exercises.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
-    id INT UNSIGNED AUTO_INCREMENT,
-    artist CHAR(50),
-    album_name CHAR(50),
-    release_date DATE,
-    sales DECIMAL(4,2),
-    genre CHAR(50),
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(128) NOT NULL DEFAULT 'Unknown',
+    name VARCHAR(128) NOT NULL DEFAULT 'Untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
+    genre VARCHAR(255),
     PRIMARY KEY(id)
 );
