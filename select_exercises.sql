a640ca3a96c5c8f1009fdae7cadadab226ff627a
Correy Everett <correy.everett@gmail.com>
2021-01-11 16:49:25 -0600

    SELECT - All songs that sold less than 20 million

--- select_exercises.sql
+++ select_exercises.sql
@@ -14,3 +14,13 @@ SELECT 'Genre of the song Nevermind' AS '';
 SELECT genre
 FROM albums
 WHERE name = 'Nevermind';
+
+SELECT 'Released in the 1990''s' AS '';
+SELECT name
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Less than 20 million in sales' AS '';
+SELECT name
+FROM albums
+WHERE sales < 20;
