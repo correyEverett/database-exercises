e3777722d2065ace1fb2c603178dcd18ab64b4c0
Correy Everett <correy.everett@gmail.com>
2021-01-11 12:12:54 -0600

    create table - albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,3 +1,13 @@
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+    id INT UNSIGNED AUTO_INCREMENT,
+    artist CHAR(50),
+    album_name CHAR(50),
+    release_date DATE,
+    sales DECIMAL(4,2),
+    genre CHAR(50),
+    PRIMARY KEY(id)
+);
