5ca515a751bfd52caacdd19fd30ece0a777636f5
Correy Everett <correy.everett@gmail.com>
2021-01-12 10:35:36 -0600

    SELECT - 3

--- update_exercises.sql
+++ update_exercises.sql
@@ -6,3 +6,7 @@ FROM albums;
 SELECT id, name AS 'Released Before 1980'
 FROM albums
 WHERE release_date < 1980;
+
+SELECT id, name AS 'Michael Jackson Albums'
+FROM albums
+WHERE artist = 'Michael Jackson' ;
