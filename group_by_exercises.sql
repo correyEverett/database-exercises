4f13585fae0136faa9ff644c6a0677d6694d559d
Correy Everett <correy.everett@gmail.com>
2021-01-14 10:23:41 -0600

    find unique combinations of first and last name where the last name starts and ends with E

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -5,3 +5,7 @@ SELECT DISTINCT title FROM titles;
 SELECT last_name FROM employees
 WHERE last_name LIKE 'e%e'
 GROUP BY last_name;
+
+SELECT last_name, first_name FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name, first_name;
